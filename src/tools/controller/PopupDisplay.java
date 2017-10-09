c31df95a5ac420701f7c4d2e5c072a1756d667ab
X1Warrior <zsto7638@gmail.com>
2017-10-09 13:29:16 -0600

    Started to set up a GUI class using JOptionPane

--- src/tools/controller/PopupDisplay.java
+++ src/tools/controller/PopupDisplay.java
@@ -1,6 +1,17 @@
 package tools.controller;
 
+import javax.swing.JOptionPane;
+
 public class PopupDisplay
 {
 
+	public void displayText(String textToDisplay)
+	{
+		JOptionPane.showMessageDialog(null,  textToDisplay);
+	}
+	
+	public String getResponse(String fromQuestion)
+	{
+		String answer = "";
+	}
 }
